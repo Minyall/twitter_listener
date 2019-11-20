5a1d5008ce81475a24f91153806bb5ffecff1392
Minyall <minyall@gmail.com>
2019-11-20 23:18:51 +0000

    reshaped the credentials file

--- credentials.py
+++ credentials.py
@@ -1,4 +1,62 @@
-CONSUMER_KEY = ''
-CONSUMER_SECRET = ''
-ACCESS_SECRET = ''
-ACCESS_KEY = ''
+CREDENTIALS = [
+    {'consumer_key': 'key_here',
+     'consumer_secret': 'secret_here',
+     'access_key': 'key_here',
+     'access_secret': 'secret_here'},
+
+    {'consumer_key': 'key_here',
+     'consumer_secret': 'secret_here',
+     'access_key': 'key_here',
+     'access_secret': 'secret_here'},
+
+    # {'consumer_key': 'key_here',
+    #  'consumer_secret': 'secret_here',
+    #  'access_key': 'key_here',
+    #  'access_secret': 'secret_here'},
+    #
+    # {'consumer_key': 'key_here',
+    #  'consumer_secret': 'secret_here',
+    #  'access_key': 'key_here',
+    #  'access_secret': 'secret_here'},
+    #
+    # {'consumer_key': 'key_here',
+    #  'consumer_secret': 'secret_here',
+    #  'access_key': 'key_here',
+    #  'access_secret': 'secret_here'},
+    #
+    # {'consumer_key': 'key_here',
+    #  'consumer_secret': 'secret_here',
+    #  'access_key': 'key_here',
+    #  'access_secret': 'secret_here'},
+    #
+    # {'consumer_key': 'key_here',
+    #  'consumer_secret': 'secret_here',
+    #  'access_key': 'key_here',
+    #  'access_secret': 'secret_here'},
+    #
+    # {'consumer_key': 'key_here',
+    #  'consumer_secret': 'secret_here',
+    #  'access_key': 'key_here',
+    #  'access_secret': 'secret_here'},
+    #
+    # {'consumer_key': 'key_here',
+    #  'consumer_secret': 'secret_here',
+    #  'access_key': 'key_here',
+    #  'access_secret': 'secret_here'},
+    #
+    # {'consumer_key': 'key_here',
+    #  'consumer_secret': 'secret_here',
+    #  'access_key': 'key_here',
+    #  'access_secret': 'secret_here'},
+    #
+    # {'consumer_key': 'key_here',
+    #  'consumer_secret': 'secret_here',
+    #  'access_key': 'key_here',
+    #  'access_secret': 'secret_here'},
+    #
+    # {'consumer_key': 'key_here',
+    #  'consumer_secret': 'secret_here',
+    #  'access_key': 'key_here',
+    #  'access_secret': 'secret_here'},
+
+]
