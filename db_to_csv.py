92473bbf2e43b48e258c3347ba7a2d01ec44c32b
James Allen-Robertson <minyall@gmail.com>
2019-11-22 12:13:28 +0000

    Restored DB functions to db_to_csv

--- db_to_csv.py
+++ db_to_csv.py
@@ -1,7 +1,33 @@
+import json
+
+import dataset
+import pandas as pd
+
+def unstringify_nested_dict(df, col):
+    if col in df.columns:
+        unpacked = df[df[col].notna()][col].apply(lambda x: pd.Series(json.loads(x)))
+        unpacked = unpacked.add_prefix(col + '.')
+        df.drop(columns=[col], inplace=True)
+        df = df.merge(unpacked, left_index=True, right_index=True, how='left')
+    return df
+
+
+def load_as_dataframe(dbname, cols_to_unpack=None):
+    if not dbname.endswith('.db'):
+        dbname = dbname+'.db'
+    db = dataset.connect(f"sqlite:///{dbname}")
+    result = db['tweets'].all()
+    df = pd.DataFrame(result)
+    if cols_to_unpack != None:
+
+        for col in cols_to_unpack:
+            df = unstringify_nested_dict(df, col)
+
+    return df
+
 
 if __name__ == 'main':
     import argparse
-    from functions import load_as_dataframe
 
     parser = argparse.ArgumentParser(description='A general pupose Twitter Monitor')
     parser.add_argument('-db', '--dbname', action='store', type=str, required=True)
@@ -13,13 +39,5 @@ if __name__ == 'main':
 
     df = load_as_dataframe(dbname=dbname, cols_to_unpack=cols_to_unpack)
     df.to_csv(dbname+".csv", index=True)
-
-
-
-
-
-
-
-
 
 
